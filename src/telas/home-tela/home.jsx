dc93c800a231f6801a47f7383712a9eef91a7d3a
amissRobim Dutra <diegodutradoamaral616@gmail.com>
2026-04-14 23:47:25 -0300

    tela_pda

--- src/telas/home-tela/home.jsx
+++ src/telas/home-tela/home.jsx
@@ -1,16 +1,54 @@
+import { useState } from "react";
 import "./home.css";
-import banner from "../../assets/imagem/Subnautica.jpg"
-
+import video from "../../assets/video/Boot_subnautica.mp4";
+import banner from "../../assets/imagem/Subnautica.jpg";
 
 function Home() {
+  const [started, setStarted] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [imgLoaded, setImgLoaded] = useState(false);
+  const [fade, setFade] = useState(false);
+
+  const handleStart = () => {
+    setStarted(true);
+    setLoading(true);
+  };
+
+  const handleEnd = () => {
+    setFade(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  };
+
   return (
     <div>
-      <div className="Image-fundo">
-        <img src={banner}/>
-      </div>
-      <section className="bem-vindo">
+      {!started && (
+        <div className="start-screen" onClick={handleStart}>
+          <h1>Click para iniciar o PDA</h1>
+        </div>
+      )}
 
-      </section>
+      {loading && (
+        <div className={`loading ${fade ? "sumir" : ""}`}>
+          <video autoPlay onEnded={handleEnd} className="video">
+            <source src={video} type="video/mp4" />
+          </video>
+        </div>
+      )}
+
+      {started && !loading && (
+        <>
+          <div className="Image-fundo">
+            <img
+              src={banner}
+              className={imgLoaded ? "img loaded" : "img"}
+              onLoad={() => setImgLoaded(true)}
+            />
+          </div>
+          <section className="bem-vindo"></section>
+        </>
+      )}
     </div>
   );
 }
